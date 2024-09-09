d9cce25b651269757f6b6d9964e72ce76afe14cc
jimoh4tech <olamide14044@gmail.com>
2024-09-09 09:00:40 +0100

    fix(slug): fix loading time error due to vercel free version

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,15 +1,18 @@
-// import { Call, Email, WhatsApp } from '@mui/icons-material';
-// import { Button, Grid } from '@mui/material';
-// import { Box, Container, Stack } from '@mui/system';
+import { Call, Email, WhatsApp } from '@mui/icons-material';
+import { Button, Grid } from '@mui/material';
+import { Box, Container, Stack } from '@mui/system';
 // import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
-// import Head from 'next/head';
-// import { IUserBusiness } from 'src/@types/business';
-// import CompactLayout from 'src/layouts/compact/CompactLayout';
-// import BusinesseAbout from 'src/sections/@dashboard/web-address/BusinessAbout';
-// import BusinessCover from 'src/sections/@dashboard/web-address/BusinessCover';
-// import BusinessSocialInfo from 'src/sections/@dashboard/web-address/BusinessSocialInfo';
-// import axiosInstance from 'src/utils/axios';
-// import Page404 from './404';
+import Head from 'next/head';
+import { IUserBusiness } from 'src/@types/business';
+import CompactLayout from 'src/layouts/compact/CompactLayout';
+import BusinesseAbout from 'src/sections/@dashboard/web-address/BusinessAbout';
+import BusinessCover from 'src/sections/@dashboard/web-address/BusinessCover';
+import BusinessSocialInfo from 'src/sections/@dashboard/web-address/BusinessSocialInfo';
+import axiosInstance from 'src/utils/axios';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import LoadingScreen from 'src/components/loading-screen';
+import Page404 from './404';
 
 // export const getServerSideProps: GetServerSideProps<{
 //   business: IUserBusiness | null;
@@ -111,17 +114,87 @@
 //   );
 // }
 
-import { useRouter } from 'next/router';
-
-const SlugPage = () => {
+const Page = () => {
   const router = useRouter();
+  const [business, setBusiness] = useState<IUserBusiness | null>();
   const { slug } = router.query;
+  const [errorCode, setErrorCode] = useState();
+
+  const fetchBusinessData = async () => {
+    try {
+      const { data } = await axiosInstance.get(`/web-address/${slug}`);
+      setBusiness(data);
+    } catch (error) {
+      console.log(error);
+      setErrorCode(error?.statusCode);
+    }
+  };
+  useEffect(() => {
+    fetchBusinessData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (!business) return <LoadingScreen />;
+  if (errorCode)
+    return (
+      <CompactLayout>
+        <Page404 />
+      </CompactLayout>
+    );
 
   return (
-    <div>
-      <h1>Dynamic Slug: {slug}</h1>
-    </div>
+    <>
+      <Head>
+        <title> {business?.name} | Thrive</title>
+      </Head>
+
+      <Box
+        sx={{
+          mb: 3,
+          height: 280,
+          position: 'relative',
+        }}
+      >
+        <BusinessCover isPublic business={business} />
+      </Box>
+      <Container>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={4}>
+            <Stack spacing={3}>
+              {/* <BusinessCounter follower={10000} following={300} /> */}
+
+              <BusinessSocialInfo
+                socialLinks={{
+                  facebookLink: business?.facebookLink,
+                  instagramLink: business?.instagramLink,
+                  linkedinLink: business?.linkedinLink,
+                  twitterLink: business?.twitterLink,
+                }}
+              />
+              <Button href={`tel:${business?.phone}`} variant="contained" startIcon={<Call />}>
+                Call Us Now
+              </Button>
+              <Button href={`mailto:${business?.email}`} variant="outlined" startIcon={<Email />}>
+                Send Us an Email
+              </Button>
+              <Button
+                href={`https://wa.me/${business?.whatsappNumber || business?.phone}`}
+                variant="soft"
+                color="success"
+                startIcon={<WhatsApp />}
+              >
+                Send a Whatsapp Message
+              </Button>
+            </Stack>
+          </Grid>
+
+          <Grid item xs={12} md={8}>
+            <BusinesseAbout business={business} />
+          </Grid>
+        </Grid>
+      </Container>
+    </>
   );
 };
 
-export default SlugPage;
+export default Page;
